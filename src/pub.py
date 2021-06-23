59182fe7edad88053b8773f79151277e1ddc5d0d
Connor <connorjrobertson@hotmail.co.uk>
2021-06-23 11:31:23 +0100

    First function working, yay!

--- src/pub.py
+++ src/pub.py
@@ -4,6 +4,9 @@ class Pub:
         self.till = till
         self.drinks = drinks
 
+    def take_payment(self,amount):
+        self.till += amount
+
 class Drinks:
     def __init__(self, name, price):
         self.name = name
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -4,7 +4,11 @@ from src.pub import *
 class TestPub(unittest.TestCase):
     
     def setUp(self):
-        self.pub = Pub("The Yawning Portal", 100.00)
+        self.pub = Pub("The Yawning Portal", 100.00, [])
 
     def test_pub_has_name(self):
         self.assertEqual("The Yawning Portal", self.pub.name)
+
+    def test_increase_til(self):
+        self.pub.take_payment(10)
+        self.assertEqual(110, self.pub.till)
